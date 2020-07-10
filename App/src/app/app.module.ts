4979f918d851b4d32b538840e6074fd114b6ba47
Luis Marques <luisserafim99@gmail.com>
2020-07-10 19:00:25 +0100

    Home page

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './controllers/home/home.component';
+import { DashboardComponent } from './controllers/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
