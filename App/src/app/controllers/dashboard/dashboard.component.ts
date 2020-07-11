1e336b661b8de7457958964712ac15920a3bc5bc
Luis Marques <luisserafim99@gmail.com>
2020-07-11 22:49:11 +0100

    Dashboard update

--- App/src/app/controllers/dashboard/dashboard.component.ts
+++ App/src/app/controllers/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  saftFile: any;
+  accounts: any;
+  customers: any;
+  suppliers: any;
+  products:any;
+  taxes:any;
+
+  constructor(public rest: RestService) { }
 
   ngOnInit(): void {
+    this.rest.getSaftFile().subscribe((data: {}) => {
+      console.log(data);
+      this.saftFile = data;
+      this.accounts = this.saftFile.data.MasterFiles[0].GeneralLedgerAccounts[0].Account;
+      this.customers = this.saftFile.data.MasterFiles[0].Customer;
+      this.suppliers = this.saftFile.data.MasterFiles[0].Supplier;
+      this.products = this.saftFile.data.MasterFiles[0].Product;
+      this.taxes = this.saftFile.data.MasterFiles[0].TaxTable[0].TaxTableEntry;
+    });
   }
 
 }
---
+++ App/src/app/services/rest.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { SaftFile } from "../Models/SaftFile";
+import { Observable } from 'rxjs';
+
+const endpoint = "http://localhost:5000/api/v1";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(private http: HttpClient) { };
+
+  private extraData(res: Response) {
+    let body = res;
+    return body || {};
+  };
+
+  getSaftFile(): Observable<SaftFile> {
+    return this.http.get<SaftFile>(`${endpoint}/saftFiles/5f05dc60d07e6749f8560a6e`);
+  };
+}
