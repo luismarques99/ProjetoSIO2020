5efd1d04e2de8f92066373eb5c8008d744e4fd58
Luis Marques <luisserafim99@gmail.com>
2020-07-13 17:35:39 +0100

    Dashboard component update

--- App/src/app/controllers/dashboard/dashboard.component.ts
+++ App/src/app/controllers/dashboard/dashboard.component.ts
@@ -9,14 +9,6 @@ import { RestService } from 'src/app/services/rest.service';
 export class DashboardComponent implements OnInit {
 
   saftFile: any;
-  accounts: any;
-  customers: any;
-  suppliers: any;
-  products: any;
-  taxes: any;
-  purchases: any;
-  sales: any;
-  salesInvoices: any;
 
   constructor(public rest: RestService) { }
 
@@ -24,14 +16,6 @@ export class DashboardComponent implements OnInit {
     this.rest.getSaftFile().subscribe((data: {}) => {
       console.log(data);
       this.saftFile = data;
-      this.accounts = this.saftFile.data.MasterFiles[0].GeneralLedgerAccounts[0].Account;
-      this.customers = this.saftFile.data.MasterFiles[0].Customer;
-      this.suppliers = this.saftFile.data.MasterFiles[0].Supplier;
-      this.products = this.saftFile.data.MasterFiles[0].Product;
-      this.taxes = this.saftFile.data.MasterFiles[0].TaxTable[0].TaxTableEntry;
-      this.purchases = this.saftFile.data.GeneralLedgerEntries[0].Journal[0].Transaction;
-      this.sales = this.saftFile.data.GeneralLedgerEntries[0].Journal[1].Transaction;
-      this.salesInvoices = this.saftFile.data.SourceDocuments[0].SalesInvoices[0].Invoice;
     });
   }
 }
