26aa7f01a602168c66e46a04692128385bfd21c9
Luis Marques <luisserafim99@gmail.com>
2020-07-13 17:35:52 +0100

    App routing update

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -3,6 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './controllers/home/home.component';
 import { DashboardComponent } from './controllers/dashboard/dashboard.component';
 import { AboutComponent } from './controllers/about/about.component';
+import { TablesPurchasesComponent } from './controllers/tables-purchases/tables-purchases.component';
+import { TablesSalesComponent } from './controllers/tables-sales/tables-sales.component';
+import { TablesSalesInvoicesComponent } from './controllers/tables-sales-invoices/tables-sales-invoices.component';
+import { TablesAccountsComponent } from './controllers/tables-accounts/tables-accounts.component';
+import { TablesCustomersComponent } from './controllers/tables-customers/tables-customers.component';
+import { TablesSuppliersComponent } from './controllers/tables-suppliers/tables-suppliers.component';
+import { TablesProductsComponent } from './controllers/tables-products/tables-products.component';
+import { TablesTaxesComponent } from './controllers/tables-taxes/tables-taxes.component';
 
 
 const routes: Routes = [
@@ -13,6 +21,43 @@ const routes: Routes = [
   {
     path: "dashboard",
     component: DashboardComponent
+  },
+  {
+    path: "tables",
+    children: [
+      {
+        path: "purchases",
+        component: TablesPurchasesComponent
+      },
+      {
+        path: "sales",
+        component: TablesSalesComponent
+      },
+      {
+        path: "sales-invoices",
+        component: TablesSalesInvoicesComponent
+      },
+      {
+        path: "accounts",
+        component: TablesAccountsComponent
+      },
+      {
+        path: "customers",
+        component: TablesCustomersComponent
+      },
+      {
+        path: "suppliers",
+        component: TablesSuppliersComponent
+      },
+      {
+        path: "products",
+        component: TablesProductsComponent
+      },
+      {
+        path: "taxes",
+        component: TablesTaxesComponent
+      }
+    ]
   },
   {
     path: "about",
--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -8,6 +8,14 @@ import { HomeComponent } from './controllers/home/home.component';
 import { DashboardComponent } from './controllers/dashboard/dashboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './controllers/about/about.component';
+import { TablesPurchasesComponent } from './controllers/tables-purchases/tables-purchases.component';
+import { TablesSalesComponent } from './controllers/tables-sales/tables-sales.component';
+import { TablesSalesInvoicesComponent } from './controllers/tables-sales-invoices/tables-sales-invoices.component';
+import { TablesAccountsComponent } from './controllers/tables-accounts/tables-accounts.component';
+import { TablesCustomersComponent } from './controllers/tables-customers/tables-customers.component';
+import { TablesSuppliersComponent } from './controllers/tables-suppliers/tables-suppliers.component';
+import { TablesProductsComponent } from './controllers/tables-products/tables-products.component';
+import { TablesTaxesComponent } from './controllers/tables-taxes/tables-taxes.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +23,15 @@ import { AboutComponent } from './controllers/about/about.component';
     HomeComponent,
     DashboardComponent,
     NavbarComponent,
-    AboutComponent
+    AboutComponent,
+    TablesPurchasesComponent,
+    TablesSalesComponent,
+    TablesSalesInvoicesComponent,
+    TablesAccountsComponent,
+    TablesCustomersComponent,
+    TablesSuppliersComponent,
+    TablesProductsComponent,
+    TablesTaxesComponent
   ],
   imports: [
     BrowserModule,
